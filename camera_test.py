511a1c4805d97c9881e81c8e3c5042c313b5bfbe
kazme-egawa <arnerican.f0otboy@gmail.com>
2017-11-19 05:24:38 +0900

    fix camera.py

--- camera_test.py
+++ camera_test.py
@@ -1,8 +1,12 @@
 import time
 import picamera
+from PIL import Image
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
     time.sleep(2)
     camera.capture('foo.jpg')
+
+im = Image.open("foo.jpg")
+im.show()
