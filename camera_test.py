0a528c4731e857d19e14b0a02ade6ac961042426
kazme-egawa <arnerican.f0otboy@gmail.com>
2017-11-19 05:21:44 +0900

    fix camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -4,5 +4,5 @@ import picamera
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
-    time.sleep(2) #カメラ初期化
+    time.sleep(2)
     camera.capture('foo.jpg')
