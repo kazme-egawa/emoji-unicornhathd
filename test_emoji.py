e9c42be89ebcbfff6e49fc23a0ffb0d5b1ef1fd4
kazme-egawa <arnerican.f0otboy@gmail.com>
2017-11-20 04:28:37 +0900

    fix

--- test_emoji.py
+++ test_emoji.py
@@ -2,8 +2,7 @@
 import os
 import numpy as np
 import time
-import pygame
-from pygame.locals import *
+import sys
 
 try:
     import unicornhathd
@@ -11,6 +10,40 @@ try:
 except ImportError:
     from unicorn_hat_sim import unicornhathd
 
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
 
 print("""Unicorn HAT HD: Emoji
 
@@ -27,6 +60,7 @@ unicornhathd.rotation(0)
 
 try:
     while True:
+        print i
         R = np.load('rgb/' + emoji[i] + '_R.npy')
         G = np.load('rgb/' + emoji[i] + '_G.npy')
         B = np.load('rgb/' + emoji[i] + '_B.npy')
@@ -35,18 +69,21 @@ try:
                 unicornhathd.set_pixel(x, y, R[x][y], G[x][y], B[x][y])
         unicornhathd.show()
 
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    if i == 0:
-                        i = num -1
-                    else:
-                        i -= 1
-                if event.key == K_RIGHT:
-                    if i == num - 1:
-                        i = 0
-                    else:
-                        i += 1
+        getch = _Getch()
+        x = getch()
+
+        if (x == 'a'):
+            if i == 0:
+                i = num -1
+            else:
+                i -= 1
+        elif (x == 'd'):
+            if i == num - 1:
+                i = 0
+            else:
+                i += 1
+        else:
+            sys.exit()
 
 except KeyboardInterrupt:
     unicornhathd.off()
